921085f3d0edd479f176b18fac5277ff67a6b34f
PraveenAnil <praveendosth@gmail.com>
2022-05-12 00:32:35 +0530

    Update script01.sh

--- automation/script01.sh
+++ automation/script01.sh
@@ -31,31 +31,10 @@ sudo yum install mariadb-server mariadb -y
 sudo systemctl enable mariadb
 sudo systemctl start mariadb
 
-## Secure the MariaDB installation
-sudo mysql_secure_installation <<EOF
-y
-$mssqlpass
-$mssqlpass
-y
-y
-y
-y
-EOF
+## Replace value in Script02
+sed -i "s|mssqlpass|$mssqlpass|" script02.sh
+sed -i "s|wikiPassword|$wikiPassword|" script02.sh
 
-## Create a database and a database user for MediaWiki
-sudo mysql -u root --password=$mssqlpass -e "CREATE USER 'wiki'@'localhost' IDENTIFIED BY '$wikiPassword';"#replace "wiki" and "Testing12345" with your desired credential
-sudo mysql -u root --password=$mssqlpass -e "CREATE DATABASE wikidatabase;"  
-sudo mysql -u root --password=$mssqlpass -e "GRANT ALL PRIVILEGES ON wikidatabase.* TO 'wiki'@'localhost';"
-sudo mysql -u root --password=$mssqlpass -e "FLUSH PRIVILEGES;"
+sudo bash script02.sh
 
-## Download and Extract the MediaWiki Files
-cd /home
-sudo wget https://releases.wikimedia.org/mediawiki/1.37/mediawiki-1.37.2.tar.gz
-sudo mv mediawiki-1.37.2.tar.gz /var/www/html
-cd /var/www/html/
-sudo tar xvzf /var/www/html/mediawiki-1.37.2.tar.gz
-mv /var/www/html/mediawiki-1.37.2 /var/www/html/w
-
-## Restart apache service
-sudo systemctl restart httpd
 
