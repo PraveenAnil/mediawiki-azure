fc08a21e318a11418049bf45013d582cc74f339d
PraveenAnil <praveendosth@gmail.com>
2022-05-12 00:47:36 +0530

    Update script02.sh

--- automation/script02.sh
+++ automation/script02.sh
@@ -17,13 +17,4 @@ sudo mysql -u root --password=mssqlpass -e "CREATE DATABASE wikidatabase;"
 sudo mysql -u root --password=mssqlpass -e "GRANT ALL PRIVILEGES ON wikidatabase.* TO 'wiki'@'localhost';"
 sudo mysql -u root --password=mssqlpass -e "FLUSH PRIVILEGES;"
 
-## Download and Extract the MediaWiki Files
-cd /home
-sudo wget https://releases.wikimedia.org/mediawiki/1.37/mediawiki-1.37.2.tar.gz
-sudo mv mediawiki-1.37.2.tar.gz /var/www/html
-cd /var/www/html/
-sudo tar xvzf /var/www/html/mediawiki-1.37.2.tar.gz
-mv /var/www/html/mediawiki-1.37.2 /var/www/html/w
 
-## Restart apache service
-sudo systemctl restart httpd
